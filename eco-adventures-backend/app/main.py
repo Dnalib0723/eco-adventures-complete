550837f918dd17508fce11eb70753292df55e556
Dnalib0723 <dnalib@gmail.com>
2026-01-20 12:34:23 +0800

    Update CORS for eehcepb.com

--- eco-adventures-backend/app/main.py
+++ eco-adventures-backend/app/main.py
@@ -26,6 +26,8 @@ app.add_middleware(
         "http://127.0.0.1:5173",
         "http://127.0.0.1:3000",
         "http://127.0.0.1:8080",
+        "https://eehcepb.com", 
+        "https://www.eehcepb.com",
     ],
     allow_credentials=True,
     allow_methods=["*"],
